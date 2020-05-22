2eb6b0577bc14b6d94e063d634009639c1721651
Annie Di Persio <anniedipersio@gmail.com>
2020-05-22 17:17:51 -0500

    update

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,6 +1,6 @@
 var myMap = L.map("map", {
-    center: [40.7, -73.95],
-    zoom: 11
+    center: [39.83, -98.58],
+    zoom: 5
   });
   
 
@@ -8,7 +8,7 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "mapbox.streets",
-    accessToken: API_KEY
+    accessToken: "pk.eyJ1IjoiYWRpcGVyc2lvIiwiYSI6ImNrOXpzZDNwZzBpZDYzbm92MHV6d2lidDkifQ.gL-yKZvvu0SwKSI0Wcs9cg"
 }).addTo(myMap);
   
 
@@ -16,21 +16,26 @@ url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojso
 
 d3.json(url, function(response) {
 
-    var markers = L.markerClusterGroup();
-  
-    for (var i = 0; i < response.length; i++) {
-  
-      var location = response[i].location;
-  
-      if (location) {
-  
-        markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
-          .bindPopup(response[i].descriptor));
-      }
-  
+  // Create a new marker cluster group
+  var markers = L.markerClusterGroup();
+
+  // Loop through data
+  for (var i = 0; i < response.length; i++) {
+
+    // Set the data location property to a variable
+    var location = response[i].geometry;
+
+    // Check for location property
+    if (location) {
+
+      // Add a new marker to the cluster group and bind a pop-up
+      markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
+        .bindPopup(response[i].descriptor));
     }
-  
-    myMap.addLayer(markers);
-  
-  });
-  
+
+  }
+
+  // Add our marker cluster layer to the map
+  myMap.addLayer(markers);
+
+});
